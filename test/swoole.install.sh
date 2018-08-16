33bc39f349dc1d3a6cd06f9a656f82ab3e7cec0f
李铭昕 <715557344@qq.com>
2018-08-16 09:51:31 +0800

    Fix

--- test/swoole.install.sh
+++ test/swoole.install.sh
@@ -5,7 +5,7 @@ tar -xf swoole.tar.gz -C swoole --strip-components=1
 rm swoole.tar.gz
 cd swoole
 phpize
-./configure --enable-async-redis --enable-openssl --enable-http2
+./configure --enable-async-redis --enable-openssl
 make -j$(nproc)
 make install
 cd -
